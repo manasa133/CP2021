91309bf27be22fa9b34ac4cd9850133eebf6acf5
manasa133 <manasa_a@msitprogram.net>
2021-06-11 13:58:19 +0530

    has duplicate

---
+++ 03-kth_occurrences-Python/kth_occurrences.py
@@ -0,0 +1,26 @@
+# Given a string str and an integer K, the task is to find the K-th most 
+# frequent character in the string. If there are multiple characters that 
+# can account as K-th the most frequent character then, print any one of them.
+
+
+
+def fun_kth_occurrences(s,n):
+	freq_dict = dict()
+	for i in s.lower():
+		if i not in freq_dict:
+			freq_dict[i] = 1
+		else:
+			freq_dict[i] += 1
+
+	freq_list = sorted(set(freq_dict.values()), reverse=True)
+
+	if n > len(freq_list):
+		return None
+
+	res = ""
+	for key, value in freq_dict.items():
+		if value == freq_list[n-1]:
+			res = key
+	return res
+
+print(fun_kth_occurrences("asuszenphonemaxm1 aemnsh", 6,))
